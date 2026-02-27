c825910844556bcab5e45e44479a26ac805d5ed2
jameslefevre <lef20002@byui.edu>
2026-02-27 11:14:20 -0700

    Started Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Type your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        if (grade < 60)
+        {
+            Console.WriteLine("Your grade is a F.");
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            Console.WriteLine("Your grade is a D.");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            Console.WriteLine("Your grade is a C.");
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            Console.WriteLine("Your grade is an B.");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is an A.");
+        }
+
     }
 }
