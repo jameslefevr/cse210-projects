c7f12f229a68b366d20dc8071319ed41333c2817
jameslefevre <lef20002@byui.edu>
2026-03-02 17:10:45 -0700

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            numbers.Add(number);
+        } while (number != 0);
+
+        numbers.RemoveAt(numbers.Count() - 1);
+
+        int sum = numbers.Sum();
+        double mean = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {mean}");
+        Console.WriteLine($"The largest number is: {max}");
+
+
+
     }
 }
