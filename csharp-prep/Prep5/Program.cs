d480d49735f7b6df427eac6b274661a04534dd45
jameslefevre <lef20002@byui.edu>
2026-03-11 11:44:07 -0600

    Finshed Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,11 +1,21 @@
 using System;
 using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        DisplayWelcome();
         
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int sqNumber = SquareNumber(number);
+
+        int year;
+        PromptUserBirthYear(out year);
+
+        DisplayResult(name, sqNumber, year);
     }
 
     static void DisplayWelcome()
@@ -25,6 +35,26 @@ class Program
         Console.Write("Please enter your favorite number: ");
         int favoriteNumber = int.Parse(Console.ReadLine());
         return favoriteNumber;
+    }
+
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("Please enter the year you were born: ");
+        year = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        int numberSquared = number * number;
+        return numberSquared;
+    }
+
+    static void DisplayResult(string name, int number, int year )
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}.");
+
+        int age = 2026 - year;
+        Console.WriteLine($"{name}, you will turn {age} this year.");
     }
 
 
